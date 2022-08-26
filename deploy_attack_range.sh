6281129483047600a828d9edb639c6e5fee5f306
appdyn <kevin.la@appliedynamics.com>
2022-08-26 19:18:09 +0000

    updating deployment scripts

--- deploy_attack_range.sh
+++ deploy_attack_range.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get update 
-sudo apt-get install -y python3-dev linux-headers-generic python-dev unzip python-pip vagrant virtualbox virtualbox-dkms python-virtualenv git
+sudo apt-get install -y python3-dev linux-headers-generic unzip python-pip vagrant virtualbox virtualbox-dkms python-virtualenv git #remove python-dev 
 sudo gem install winrm-elevated
 sudo gem install winrm
-git clone https://github.com/splunk/attack_range_local && cd attack_range_local
-virtualenv -p python3 venv
-source venv/bin/activate
+#git clone https://github.com/splunk/attack_range_local && cd attack_range_local
+#virtualenv -p python3 venv
+python3 -m venv attack_range 
+source attck_range/bin/activate
 pip install -r requirements.txt
 ansible-galaxy collection install community.windows
